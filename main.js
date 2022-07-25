ef4dc53962672cfe6d6d414c4664d53216a4e00e
Parivesh2005 <108748552+Parivesh2005@users.noreply.github.com>
2022-07-25 09:24:28 -0600

    Added Functions

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 // My Contacts Basic
+// localStorage.setItem("contacts", "[]")
+
 
 // HTML Elements
 let goBtnEl = document.getElementById('go-btn');
 let menuEl = document.getElementById('menu');
 let outputEl = document.getElementById('output');
+
+// Global Variables
+let contacts = loadContacts();
+displayContacts();
 
 // Go Btn - Menu Listener
 goBtnEl.addEventListener('click', goBtnHandler);
@@ -27,21 +33,81 @@ function goBtnHandler() {
 
 // MENU FUNCTIONS
 function displayContacts() {
-  console.log('Display Contacts');
+  let outputStr = '';
+  for (let i = 0; i < contacts.length; i++){
+    outputStr += getContactHTMLStr(contacts[i], i);
+  }
+  outputEl.innerHTML = outputStr;
+  saveContacts();
 }
 
 function addContact() {
-  console.log('Add Contact');
+  let name = prompt("Enter a name");
+  let phone = prompt("Please enter a phone number");
+  let email = prompt("Please enter an email");
+  let country = prompt("Please enter your country");
+  let description = {'name': name, 'phone': phone, 'email': email, 'country':country}
+  contacts.push(newContacts(description));
+  for (let i = 0; i < contacts.length; i++){
+  outputEl.innerHTML = `New Contact Added: ${i} ${name}`
+  saveContacts();
+  }
 }
 
+// Remove contact by index
 function removeContact() {
-  console.log('Remove Contact');
+  let index = +prompt("Enter the # of contact you want to remove:");
+  if (index >= 0 && index < contacts.length) {
+    contacts.splice(index, 1);
+    saveContacts();
+    displayContacts();
+  } else {
+    alert("Number is not valid");
+  }
 }
 
 function displayByName() {
-  console.log('Display by Name');
+  outputEl.innerHTML = "";
+  let searchByName = prompt("Please search info by name");
+  for (let i = 0; i < contacts.length; i++){
+    if (contacts[i].description.name.includes(searchByName)) {
+      outputEl.innerHTML += getContactHTMLStr(contacts[i], i);
+    }
+  }
+
 }
 
 function displayByCountry() {
-  console.log('Display by Country');
+  outputEl.innerHTML = "";
+  let countrySearch = prompt("Please search a country");
+  for (let i = 0; i < contacts.length; i++) {
+  if (countrySearch === contacts[i].description.country) {
+    outputEl.innerHTML += getContactHTMLStr(contacts[i], i);
+    }
+  }
+}
+// Helper Functions
+function newContacts(contactDescription) {
+  return {
+    description: contactDescription,
+    completed: ''
+  }
+}
+
+function getContactHTMLStr(contacts, i) {
+  return `
+    <div> 
+    Contact(s): <b>${i}: ${contacts.description.name}</b> <p>${contacts.description.email}</p> ${contacts.description.phone} (${contacts.description.country})
+    </div>`
+}
+// Save global contact to local storage
+
+function saveContacts() {
+  localStorage.setItem("contacts", JSON.stringify(contacts));
+}
+
+// Load global contact to local storage
+function loadContacts() {
+  let contactStr = localStorage.getItem('contacts');
+  return JSON.parse(contactStr) ?? [];
 }
